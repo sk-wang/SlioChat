d93130b7e97a781f0fa46597266b810c1cddd56c
wanghao <wanghao@shiyanjia.com>
2025-02-18 01:07:09 +0800

    增加npm打包的逻辑

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: [
+    require('postcss-preset-env')({
+      browsers: 'last 2 versions',
+      stage: 3,
+      features: {
+        'nesting-rules': true
+      }
+    })
+  ]
+}; 
---
+++ webpack.config.js
@@ -0,0 +1,93 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              minimize: true,
+              esModule: false,
+              sources: true,
+              preprocessor: (content, loaderContext) => {
+                content = content.replace(/<script>([\s\S]*?)<\/script>/gi, (match, p1) => {
+                  return `<script>${require('babel-core').transform(p1, {
+                    presets: ['@babel/preset-env']
+                  }).code}</script>`;
+                });
+                return content;
+              }
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      inject: 'body',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true,
+        minifyCSS: true,
+        minifyJS: {
+          compress: {
+            drop_console: true,
+          },
+          mangle: true
+        }
+      }
+    })
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          parse: {
+            ecma: 8
+          },
+          compress: {
+            ecma: 5,
+            warnings: false,
+            comparisons: false,
+            inline: 2,
+            drop_console: true
+          },
+          mangle: {
+            safari10: true
+          },
+          output: {
+            ecma: 5,
+            comments: false,
+            ascii_only: true
+          }
+        },
+        extractComments: false
+      })
+    ]
+  }
+}; 
